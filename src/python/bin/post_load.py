6ec83379aa392c0e91817c4d50b3bd93b5c7965f
peterzdeb <peterzdeb@gmail.com>
2014-11-13 00:21:04 +0200

    added files linking

--- src/python/bin/post_load.py
+++ src/python/bin/post_load.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     env = os.environ
     try:
-        if env.has_key('TR_TORRENT_NAME'):
+        if env._data.has_key('TR_TORRENT_NAME'):
             torrent_file = env.get('TR_TORRENT_NAME', torrent_file)
             torrent_file = remove_xdc(torrent_file)
         if len(sys.argv) == 3:
