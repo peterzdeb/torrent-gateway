b8a6c59dfc58d9d0e8b8d9b842f960cf29b23c0a
peterzdeb <peterzdeb@gmail.com>
2014-11-17 00:43:49 +0200

    bug with error logging

--- bin/post_load.sh
+++ bin/post_load.sh
@@ -2,4 +2,4 @@
 
 PROD_ROOT=/home/transmission/torrent-gateway
 export PROD_ROOT
-exec `python3.4 $PROD_ROOT/src/python/bin/post_load.py 2>> /var/log/torrent_gw.log`
+exec `python3.4 $PROD_ROOT/src/python/bin/post_load.py $@ 2>> /var/log/torrent_gw.log`
