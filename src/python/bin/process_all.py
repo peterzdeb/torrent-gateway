d242387df7b82f061d61fad01a2723db07778e86
peterzdeb <peterzdeb@gmail.com>
2015-02-22 20:37:13 +0200

    minor bug fix

--- src/python/bin/process_all.py
+++ src/python/bin/process_all.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         else:
             torrents_dir = ''
 
-        if not torrent_dir:
+        if not torrents_dir:
             logger.error('Torrent error - no torrents dir specified')
             sys.exit(1)
 
